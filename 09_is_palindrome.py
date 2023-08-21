9649768bb2fcffd8a13ee2055561765d0c9b4d7c
ross-brown <rbrown_98@yahoo.com>
2023-08-21 18:45:33 -0500

    finish problems 9 and 10

--- 09_is_palindrome.py
+++ 09_is_palindrome.py
@@ -21,3 +21,25 @@ def is_palindrome(phrase):
         >>> is_palindrome('Noon')
         True
     """
+
+    no_space_phrase = phrase.replace(" ", "")
+    lowerd_phrase = no_space_phrase.lower()
+
+    # letters = [char for char in lowerd_phrase]
+
+    # letters.reverse()
+
+    # reversed = ''.join(letters)
+
+    # return reversed == lowerd_phrase
+
+    start = 0
+    end = len(lowerd_phrase) - 1
+
+    while start < end:
+        if not lowerd_phrase[start] == lowerd_phrase[end]:
+            return False
+        else:
+            start += 1
+            end -= 1
+    return True
