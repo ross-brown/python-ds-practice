ce153e1b291e725c3a03032abc67d0c5d9b360e1
Jung-GunSong <jsong781@gmail.com>
2023-08-21 20:18:43 -0400

    completed up to problem 15

--- 11_flip_case.py
+++ 11_flip_case.py
@@ -11,3 +11,13 @@ def flip_case(phrase, to_swap):
         'AaaaHHH'
 
     """
+    new_phrase = ""
+    lowered_to_swap = to_swap.lower()
+
+    for char in phrase:
+        if(char.lower() == lowered_to_swap):
+            new_phrase += char.swapcase()
+        else:
+            new_phrase += char
+
+    return new_phrase
